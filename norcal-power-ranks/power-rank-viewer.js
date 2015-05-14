f5952618bf0cfe851e27bd66900368e0e7a7e3d8
Shawn Krisman <shawn@everlane.com>
2015-05-14 10:16:23 -0700

    fixed issue with bad lines.

--- norcal-power-ranks/power-rank-viewer.js
+++ norcal-power-ranks/power-rank-viewer.js
@@ -9,13 +9,14 @@ var pages = [{
     type: 'unranked'
 }]
 
-var playerJSON = '[{"displayName":"1UP | X","normalizedName":"x","type":"ranked","wins":["PumpMagic","Virus","Ikez","Nanerz","Scourge","DSS"],"losses":["Ito","Trevonte"]},{"displayName":"Trevonte","normalizedName":"trevonte","type":"ranked","wins":["Phancy","Nanerz","DSS","Villain","AD"],"losses":["ZeRo","Arikie","Scourge","Villain"]},{"displayName":"UC | DSS","normalizedName":"dss","type":"ranked","wins":["TwicH","Rice","X","pumpmagic","bobatapioca","Arikie","Kronos","Nanerz"],"losses":["Zex","ito","ito","Trevonte","X"]},{"displayName":"Sean","normalizedName":"sean","type":"ranked","wins":["Waael","Legit"],"losses":["Arikie","Villain"]},{"displayName":"SilentSpectre","normalizedName":"silentspectre","type":"ranked","wins":[],"losses":["Virus","Villain"]},{"displayName":"1UP | Nitro","normalizedName":"nitro","type":"ranked","wins":["Rice","Villain","Bandt","Nitro","X","DSS"],"losses":["ZeRo","ZeRo","Trevonte"]},{"displayName":"Jeepy","normalizedName":"jeepy","type":"ranked","wins":[],"losses":[]},{"displayName":"Rice","normalizedName":"rice","type":"ranked","wins":["Kronos","Boba Tapioca"],"losses":[]},{"displayName":"TwicH","normalizedName":"twich","type":"ranked","wins":[],"losses":["DSS","Hitaku"]},{"displayName":"Scourge","normalizedName":"scourge","type":"ranked","wins":["Trevonte","Kronos"],"losses":["X"]},{"displayName":"1UP | Phancy","normalizedName":"phancy","type":"ranked","wins":["Soulimar","Solitary"],"losses":["Trevonte","Virus"]},{"displayName":"CrispyTacoz","normalizedName":"crispytacoz","type":"ranked","wins":[],"losses":["3Hai","ss3katen"]},{"displayName":"AD","normalizedName":"ad","type":"ranked","wins":[],"losses":["Bandt"]},{"displayName":"Bandt","normalizedName":"bandt","type":"ranked","wins":["AD"],"losses":["Nitro","Crow"]},{"displayName":"Hitaku","normalizedName":"hitaku","type":"ranked","wins":["TwicH","\\n"],"losses":["Virus"]},{"displayName":"Soulimar","normalizedName":"soulimar","type":"previously-ranked","wins":["Soronie"],"losses":["Phancy","Waael","superoven"]},{"displayName":"Fangfire","normalizedName":"fangfire","type":"previously-ranked","wins":[],"losses":["ChaosPro"]},{"displayName":"Boba Tapioca","normalizedName":"bobatapioca","type":"previously-ranked","wins":["fangfire","scourge","phancy","phancy"],"losses":["ZeRo","Rice","DSS"]},{"displayName":"BKO Legit","normalizedName":"bkolegit","type":"previously-ranked","wins":["Boba Tapioca"],"losses":["Ito","Sean"]},{"displayName":"Ito","normalizedName":"ito","type":"previously-ranked","wins":["pump magic","dss","Legit"],"losses":["ZeRo"]},{"displayName":"Phoenix D","normalizedName":"phoenixd","type":"previously-ranked","wins":[],"losses":["BKO Virus","Dragoomba"]},{"displayName":"Kronos2560","normalizedName":"kronos2560","type":"previously-ranked","wins":["GenD","Solitary","Nitro"],"losses":["DSS","Scourge"]},{"displayName":"Nanerz","normalizedName":"nanerz","type":"unranked","wins":["Virus","Solitary","GenD"],"losses":["Trevonte","Arikie","X","DSS"]},{"displayName":"Waael","normalizedName":"waael","type":"unranked","wins":["Soulimar"],"losses":["Zex"]},{"displayName":"Arikie","normalizedName":"arikie","type":"unranked","wins":["Scourge","Nanerz","Trevonte","Solitary","Sean"],"losses":["Ito","Zex","Zex","DSS"]},{"displayName":"BKO | Virus","normalizedName":"virus","type":"unranked","wins":["Hitaku","Phancy","Phoenix D","SilentSpectre","Scourge","Villain\\n"],"losses":["Nanerz","Zex","Ito"]},{"displayName":"1UP | Soilitary","normalizedName":"soilitary","type":"unranked","wins":["Waael","Hitaku","Nitro"],"losses":["Arikie","Nanerz","Kronos"]},{"displayName":"Soronie","normalizedName":"soronie","type":"unranked","wins":["Soulimar"],"losses":["superoven","soulimar"]},{"displayName":"ChaosPro","normalizedName":"chaospro","type":"unranked","wins":["Fangfire"],"losses":["ApologyMan"]},{"displayName":"GEN D","normalizedName":"gend","type":"unranked","wins":["Fangfire"],"losses":["ZeRo","Nanerz"]},{"displayName":"3Hai","normalizedName":"3hai","type":"unranked","wins":[],"losses":["CrispyTacoz"]}]'
+var playerJSON = '[{"displayName":"1UP | X","normalizedName":"x","type":"ranked","wins":["PumpMagic","Virus","Ikez","Nanerz","Scourge","DSS"],"losses":["Ito","Trevonte"]},{"displayName":"Trevonte","normalizedName":"trevonte","type":"ranked","wins":["Phancy","Nanerz","DSS","Villain","AD"],"losses":["ZeRo","Arikie","Scourge","Villain"]},{"displayName":"UC | DSS","normalizedName":"dss","type":"ranked","wins":["TwicH","Rice","X","pumpmagic","bobatapioca","Arikie","Kronos","Nanerz"],"losses":["Zex","ito","ito","Trevonte","X"]},{"displayName":"Sean","normalizedName":"sean","type":"ranked","wins":["Waael","Legit"],"losses":["Arikie","Villain"]},{"displayName":"SilentSpectre","normalizedName":"silentspectre","type":"ranked","wins":[],"losses":["Virus","Villain"]},{"displayName":"1UP | Nitro","normalizedName":"nitro","type":"ranked","wins":["Rice","Villain","Bandt","Nitro","X","DSS"],"losses":["ZeRo","ZeRo","Trevonte"]},{"displayName":"Jeepy","normalizedName":"jeepy","type":"ranked","wins":[],"losses":[]},{"displayName":"Rice","normalizedName":"rice","type":"ranked","wins":["Kronos","Boba Tapioca"],"losses":[]},{"displayName":"TwicH","normalizedName":"twich","type":"ranked","wins":[],"losses":["DSS","Hitaku"]},{"displayName":"Scourge","normalizedName":"scourge","type":"ranked","wins":["Trevonte","Kronos"],"losses":["X"]},{"displayName":"1UP | Phancy","normalizedName":"phancy","type":"ranked","wins":["Soulimar","Solitary"],"losses":["Trevonte","Virus"]},{"displayName":"CrispyTacoz","normalizedName":"crispytacoz","type":"ranked","wins":[],"losses":["3Hai","ss3katen"]},{"displayName":"AD","normalizedName":"ad","type":"ranked","wins":[],"losses":["Bandt"]},{"displayName":"Bandt","normalizedName":"bandt","type":"ranked","wins":["AD"],"losses":["Nitro","Crow"]},{"displayName":"Hitaku","normalizedName":"hitaku","type":"ranked","wins":["TwicH"],"losses":["Virus"]},{"displayName":"Soulimar","normalizedName":"soulimar","type":"previously-ranked","wins":["Soronie"],"losses":["Phancy","Waael","superoven"]},{"displayName":"Fangfire","normalizedName":"fangfire","type":"previously-ranked","wins":[],"losses":["ChaosPro"]},{"displayName":"Boba Tapioca","normalizedName":"bobatapioca","type":"previously-ranked","wins":["fangfire","scourge","phancy","phancy"],"losses":["ZeRo","Rice","DSS"]},{"displayName":"BKO Legit","normalizedName":"bkolegit","type":"previously-ranked","wins":["Boba Tapioca"],"losses":["Ito","Sean"]},{"displayName":"Ito","normalizedName":"ito","type":"previously-ranked","wins":["pump magic","dss","Legit"],"losses":["ZeRo"]},{"displayName":"Phoenix D","normalizedName":"phoenixd","type":"previously-ranked","wins":[],"losses":["BKO Virus","Dragoomba"]},{"displayName":"Kronos2560","normalizedName":"kronos2560","type":"previously-ranked","wins":["GenD","Solitary","Nitro"],"losses":["DSS","Scourge"]},{"displayName":"Nanerz","normalizedName":"nanerz","type":"unranked","wins":["Virus","Solitary","GenD"],"losses":["Trevonte","Arikie","X","DSS"]},{"displayName":"Waael","normalizedName":"waael","type":"unranked","wins":["Soulimar"],"losses":["Zex"]},{"displayName":"Arikie","normalizedName":"arikie","type":"unranked","wins":["Scourge","Nanerz","Trevonte","Solitary","Sean"],"losses":["Ito","Zex","Zex","DSS"]},{"displayName":"BKO | Virus","normalizedName":"virus","type":"unranked","wins":["Hitaku","Phancy","Phoenix D","SilentSpectre","Scourge","Villain\\n"],"losses":["Nanerz","Zex","Ito"]},{"displayName":"1UP | Soilitary","normalizedName":"soilitary","type":"unranked","wins":["Waael","Hitaku","Nitro"],"losses":["Arikie","Nanerz","Kronos"]},{"displayName":"Soronie","normalizedName":"soronie","type":"unranked","wins":["Soulimar"],"losses":["superoven","soulimar"]},{"displayName":"ChaosPro","normalizedName":"chaospro","type":"unranked","wins":["Fangfire"],"losses":["ApologyMan"]},{"displayName":"GEN D","normalizedName":"gend","type":"unranked","wins":["Fangfire"],"losses":["ZeRo","Nanerz"]},{"displayName":"3Hai","normalizedName":"3hai","type":"unranked","wins":[],"losses":["CrispyTacoz"]}]'
 
 $(function() {
     $.isLoading({
         position: 'overlay',
         'class': 'isloading-icon'});
     var players = JSON.parse(playerJSON);
+    completeWinLossRecords(players);
     var links = createWinnerLinks(players);
 
     createGraph(players, links);
@@ -27,6 +28,51 @@ var usersFromPage = function(page) {
         url: page.url,
         dataType: 'text',
     }).then(csvToArrays).then(function(arr) {return usersFromArrays(arr, page.type)});
+};
+
+var completeWinLossRecords = function(players) {
+    players.forEach(function(player) {
+        player.wins.forEach(function(nameOfPersonTheyBeat) {
+            var nodeOfPersonTheyBeat = findPlayerNode(nameOfPersonTheyBeat, players);
+            if (nodeOfPersonTheyBeat !== null &&
+                nodeOfPersonTheyBeat.losses.indexOf(player.displayName) === -1) {
+                nodeOfPersonTheyBeat.losses.push(player.displayName);
+            }
+        });
+        player.wins.forEach(function(nameOfPersonTheyLostTo) {
+            var nodeOfPersonTheyLostTo = findPlayerNode(nameOfPersonTheyLostTo, players);
+            if (nodeOfPersonTheyLostTo !== null &&
+                nodeOfPersonTheyLostTo.wins.indexOf(player.displayName) === -1) {
+                nodeOfPersonTheyLostTo.wins.push(player.displayName);
+            }
+        });
+    });
+    normalizeWinLossRecords(players);
+};
+
+var normalizeWinLossRecords = function(players) {
+    players.forEach(function(player) {
+        player.wins = normalizePlayerList(player.wins, players);
+        player.losses = normalizePlayerList(player.losses, players);
+    });
+};
+
+var normalizePlayerList = function(list, players) {
+    var normalizedPlayers = list.map(function(name) {
+        return normalizeName(name);
+    });
+    var isUnique = function(name, index, self) {
+        return self.indexOf(name) === index &&
+            findPlayerNode(name, players) !== null;
+    }
+    var filteredPlayers = normalizedPlayers.filter(isUnique);
+    return filteredPlayers.map(function(normalizedName) {
+        if (normalizedName === '') {
+            console.log(normalizedPlayers);
+        }
+        var node = findPlayerNode(normalizedName, players);
+        return node.displayName;
+    });
 };
 
 var createWinnerLinks = function(players) {
@@ -163,6 +209,10 @@ var playerSearcher = function(name) {
     }
 };
 
+var findPlayerNode = function(name, players) {
+    return findElement(players, playerSearcher(name));
+}
+
 var highlightPlayerByNode = function(playerNode, players) {
     highlightPlayer(playerNode.normalizedName, players);
 };
@@ -175,7 +225,7 @@ var highlightPlayer = function(player, players) {
 
     $('line[source="' + player + '"], line[target="' + player + '"]').fadeIn(FADE_TIME / 2);
 
-    var playerNode = findElement(players, playerSearcher(player));
+    var playerNode = findPlayerNode(player, players);
     var html = '<div class="centered">'
     html += '<h2 class="current-player">' + playerNode.displayName + '</h2>'
     html += playerHTML('Wins', 'wins', playerNode.wins, players);
